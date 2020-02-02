155a3b3a6e4abd595c221a4b58e0e690ef1cf02b
brandon-layton <b-layton@cox.net>
2020-02-01 18:32:11 -0700

    finished mobile view

--- weather/locations/js/responsive-menu.js
+++ weather/locations/js/responsive-menu.js
@@ -1,9 +1,12 @@
 /* Toggle between adding and removing the "responsive" class to the navbar when the user clicks on the icon */
 function responsiveMenu() {
     var x = document.getElementById("navbar");
+    var y = document.getElementById("footer");
     if (x.className === "hidden") {
       x.className = "shown";
+      y.className = "large-footer";
     } else {
       x.className = "hidden";
+      y.className = "small-footer";
     }
   } 
