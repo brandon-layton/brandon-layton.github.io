26be58c2927ed64ce356df16e97841d69e9a87e9
brandon-layton <b-layton@cox.net>
2020-02-17 18:53:52 -0700

    added blur

--- weather/docs/js/img-optimize.js
+++ weather/docs/js/img-optimize.js
@@ -3,8 +3,10 @@ let imagesToLoad = document.querySelectorAll('source[data-src]');
 
 const loadImages = (image) => {
         image.setAttribute('srcset', image.getAttribute('data-src'));
-        image.onload = () => {
+        
+        image.parentElement.lastElementChild.onload = () => {
             image.removeAttribute('data-src');
+            image.parentElement.lastElementChild.className = 'clear';
         };
 };
 
