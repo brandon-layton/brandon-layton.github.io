89491d7f55625fbef5c5f1cd2a648289577a54f3
brandon-layton <b-layton@cox.net>
2020-02-17 19:07:24 -0700

    added fancy stuff

--- weather/docs/js/img-optimize.js
+++ weather/docs/js/img-optimize.js
@@ -1,17 +1,17 @@
+//grab all sorces with atribute data-src
 let imagesToLoad = document.querySelectorAll('source[data-src]');
 
-
 const loadImages = (image) => {
+        //make the image load before displayed
         image.setAttribute('srcset', image.getAttribute('data-src'));
-        
+        //after it displays
         image.parentElement.lastElementChild.onload = () => {
             image.removeAttribute('data-src');
             image.parentElement.lastElementChild.className = 'clear';
         };
 };
 
-
-
+//call load images for each image
 imagesToLoad.forEach((source) => {
     loadImages(source);
   });
